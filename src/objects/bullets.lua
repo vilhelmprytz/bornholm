eabf73273cd1e55c4786ac32eb2645c34ca1fadf
Vilhelm Prytz <vilhelm@prytznet.se>
2020-04-21 20:27:21 +0200

    Basic bullet creation

--- src/objects/bullets.lua
+++ src/objects/bullets.lua
@@ -7,24 +7,56 @@
 bullets = {}
 
 function bullets:load()
-    bullet_objects = {}
+    bullets.default_velocity = 500
 
+    bullet_objects = {}
+end
+
+local function signum(number)
+    if number > 0 then
+       return 1
+    elseif number < 0 then
+       return -1
+    else
+       return 0
+    end
 end
 
 function bullets:update(dt)
+    for i,bullet in ipairs(bullet_objects) do
+        bullet.x = bullet.x+bullet.horizontal_velocity*dt
+        bullet.y = bullet.y+bullet.vertical_velocity*dt
+    end
+
     if love.mouse.isDown(1) then
         local x, y = love.mouse.getPosition()
 
-        print(x .. " " .. y)
-    end
+        local sx = ((love.graphics.getWidth()/2) - (player.width/2))
+        local sy = ((love.graphics.getHeight()/2) - (player.height/2))
 
+        local k = math.abs((y - sy)/(x - sx))
+
+        bullet = {}
+
+        local size = 1/(math.pow(1 + math.pow(k, 2), 0.5)) * bullets.default_velocity
+        bullet.horizontal_velocity = size * signum(x - sx)
+        bullet.vertical_velocity = k * size * signum(y - sy)
+
+        bullet.x = player.x
+        bullet.y = player.y
+
+        table.insert(bullet_objects, bullet)
+    end
 end
 
 function bullets:draw()
-    
-    --[[ love.graphics.setColor(255,255,0)
-        -- love.graphics.rectangle("line", player.x, player.y, player.width, player.height)
-    local x, y = bullets.position
-    love.graphics.draw("fill", x, y) ]]
+    local screen_width = love.graphics.getWidth()
+    local screen_height = love.graphics.getHeight()
 
+    for i,bullet in ipairs(bullet_objects) do
+        local x = bullet.x - (player.x - screen_width/2 + player.width/2)
+        local y = bullet.y - (player.y - screen_height/2 + player.height/2)
+
+        love.graphics.rectangle("fill", x, y, 5, 5)
+    end
 end
